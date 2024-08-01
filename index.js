b89ae1738bd80229b5341ae142c3b2969b8f7008
GitHub-trails <pragadeesh@servx.global>
2024-08-01 07:54:37 +0530

    U2U communication

--- index.js
+++ index.js
@@ -6,16 +6,40 @@ const io = require('socket.io')(httpServer, {
 
 const port = 3000;
 
+// Object to store socket IDs with usernames
+const users = {};
+
 io.on('connection', (socket) => {
   console.log('a user connected');
 
-  socket.on('message', (message) => {
-    console.log(message);
-    io.emit('message', `${socket.id.substr(0, 2)}: ${message}`);
+  // Listen for the 'setUsername' event to store the username
+  socket.on('setUsername', (username) => {
+    users[socket.id] = username;
+
+    // Notify all clients of the updated user list
+    io.emit('userList', users);
+  });
+
+  socket.on('message', (data) => {
+    console.log(data);
+    const { targetSocketId, message } = data;
+
+    // Send message to specific client
+    if (targetSocketId && users[targetSocketId]) {
+      io.to(targetSocketId).emit('message', `${users[socket.id]}: ${message}`);
+    } else {
+      // If no targetSocketId is provided, broadcast to all clients
+      io.emit('message', `${users[socket.id]}: ${message}`);
+    }
   });
 
   socket.on('disconnect', () => {
     console.log('a user disconnected!');
+    // Remove the user's socket ID from the users object
+    delete users[socket.id];
+
+    // Notify all clients of the updated user list
+    io.emit('userList', users);
   });
 });
 
